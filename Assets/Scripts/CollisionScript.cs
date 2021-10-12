8c890816c1199b3cf554c1693dc6ed8feb5f036a
15ShermanWoo <shermanwwj@hotmail.com>
2021-10-12 17:22:15 +0800

    Added CollisionScript
    Added Score Feature
    Added Score Counter
    Added Collision Between Play and Coin = + Score

---
+++ Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionScript : MonoBehaviour
+{
+    public int CoinScore;
+    public Text CoinCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CoinCounter.text = "Score: " + CoinScore;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            CoinScore += 10;
+            Destroy(collision.gameObject);
+            CoinCounter.text = "Score: " + CoinScore;
+        }
+    }
+}
